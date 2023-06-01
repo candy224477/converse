f3bf9e63012c29a0f8e1a293e1cdd611e290b648
Oluwatobi Ayannuga <ayannugatobi@gmail.com>
2023-06-01 11:42:37 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- <?php
+require 'header.html';
+echo "<h1>Welcome</h1>";
+include 'footer.html';
+?> -->
+ 
+
+
+<?php
+  require'header.html';
+?>
+<h1>Welcome</h1>
+<form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ; ?>" enctype="multipart/form-data">
+    <input type="file" name="upload"/> <br/>
+    <input type="submit" value="upload"/>
+</form>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $file = $_FILES['upload'];
+        $storage = "storage/";
+        $move = $storage . basename($file['name']);
+        if(!in_array($file['type'], ['video/mp4', 'video/mkv'])){
+            echo "<h1>File type not supported</h1>";
+            echo "<h1>choose an MP4 video</h1>";
+        } else{ 
+            if(move_uploaded_file($file['tmp_name'],$move)){
+                echo "<h1>File uploaded successfully</h1>";
+            }else{
+                echo "<h1>File not found</h1>";
+            };
+        }
+    }
+
+
+
+?>
